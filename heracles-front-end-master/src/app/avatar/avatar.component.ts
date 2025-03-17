2becfb1a0c520472c6654aa7cbf9dac1d6f1a638
oussema <ososmhemed@gmail.com>
2025-03-17 17:35:35 +0100

    v15

--- heracles-front-end-master/src/app/avatar/avatar.component.ts
+++ heracles-front-end-master/src/app/avatar/avatar.component.ts
@@ -1,5 +1,4 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import Hls from 'hls.js';
 
 @Component({
   selector: 'avatar',
@@ -10,21 +9,7 @@ export class AvatarComponent implements AfterViewInit {
   @ViewChild('videoPlayer') videoElement!: ElementRef;
 
   ngAfterViewInit() {
-    const video: HTMLVideoElement = this.videoElement?.nativeElement;
-    const videoSrc = 'http://localhost:8080/video/heracles.m3u8';
-
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        video.play();
-      });
-    } else if (video?.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = videoSrc;
-      video.addEventListener('loadedmetadata', () => {
-        video.play();
-      });
-    }
+    this.videoElement.nativeElement.src = 'http://localhost:8000/live/stream.m3u8';
+    this.videoElement.nativeElement.play();
   }
 }
